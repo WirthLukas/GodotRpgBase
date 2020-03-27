a7662c91f838da0c0b1cf4f38b23cd928d7106d0
Lukas Wirth <luke.wirth31@gmail.com>
2020-03-27 23:42:05 +0100

    Part 07: Background and Autotiles

--- Readme.md
+++ Readme.md
@@ -2,3 +2,10 @@
 A base for rougelike rpg games.
 
 **Based on that youtube series**: https://www.youtube.com/playlist?list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a
+
+## Background
+
+There are 2 Options:
+* TextureRect: This has more convenient options to scratch the background, but is a control node
+ and should only used by ui items
+* A sprite with a region
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -14,14 +14,6 @@ func _physics_process(delta):
 	var input_vector = get_input_vector()
 	
 	if input_vector != Vector2.ZERO:
-		# we defined a triangle, where each animation is set to a point
-		# we can assign any vector to the blend postion (it determines, which animation gets played)
-		# so we can also assign the input vector
-		# --------------------
-		# we defined Animation up and animation down to 1.1 so that animation left and right (are on 1)
-		# are prioritized
-		# --------------------
-		# these "parameters/..." are the path to the properties you can see on the inspector on the right side
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
 		animation_state.travel("Run")
@@ -31,7 +23,6 @@ func _physics_process(delta):
 		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	# returns an optimized velocity vector (for examples in edges)
 	velocity = move_and_slide(velocity)  # not + delta, because it handles this for us (see it in the docs)
 
 
