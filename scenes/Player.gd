d11ac9e52a29a7518cd23ef649a95e43af9705df
Lukas Wirth <luke.wirth31@gmail.com>
2020-03-27 19:48:33 +0100

    Part 06: Animation Tree

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,29 +1,32 @@
 extends KinematicBody2D
 
-enum Direction { UP, DOWN, LEFT, RIGHT }
-
 const ACCELERATION = 400
 const MAX_SPEED = 80
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
-var direction = Direction.RIGHT
 
 onready var animation_player = $AnimationPlayer
-
-# you have to do this, when you dont use an onready variable
-#func _ready():
-#	animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = get_input_vector()
 	
 	if input_vector != Vector2.ZERO:
-		set_direction(input_vector)
-		play_animation(direction, true)
+		# we defined a triangle, where each animation is set to a point
+		# we can assign any vector to the blend postion (it determines, which animation gets played)
+		# so we can also assign the input vector
+		# --------------------
+		# we defined Animation up and animation down to 1.1 so that animation left and right (are on 1)
+		# are prioritized
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
+
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		play_animation(direction, false)
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	# returns an optimized velocity vector (for examples in edges)
@@ -38,34 +41,3 @@ func get_input_vector():
 	input_vector = input_vector.normalized()
 	
 	return input_vector
-
-
-func set_direction(input_vector):
-	if input_vector.y > 0:
-		direction = Direction.DOWN
-	elif input_vector.y < 0:
-		direction = Direction.UP
-		
-	if input_vector.x > 0:
-		direction = Direction.RIGHT
-	elif input_vector.x < 0 :
-		direction = Direction.LEFT
-
-
-func play_animation(dir, moving):
-	var animation = ""
-	
-	match dir:
-		Direction.LEFT:
-			# Ternary operator; in other languages "condition ? x : y"
-			animation = "Run_Left" if moving else "Idle_Left"
-		Direction.RIGHT:
-			animation = "Run_Right" if moving else "Idle_Right"
-		Direction.UP:
-			animation = "Run_Up" if moving else "Idle_Up"
-		Direction.DOWN:
-			animation = "Run_Down" if moving else "Idle_Down"
-		_:
-			print("you should never ever be here!")
-	
-	animation_player.play(animation)
